8e9a6d6d9b265125ba1b6d6d4748db0e470669eb
Liam Dawson <liam@ldaws.com>
2018-12-29 21:17:07 +1100

    shift css build to function

--- build.js
+++ build.js
@@ -8,19 +8,22 @@ const CSSNano = require('cssnano');
 
 // const template = Handlebars.compile(fs.readFileSync('src/template.hbs', { encoding: 'utf8' }));
 
-/* build all css */
+function buildCSS(from, to) {
+  const cssProcessor = PostCSS(
+    PostCSSImport(),
+    PostCSSPresetEnv({ stage: 3 }),
+    CSSNano());
 
-const cssProcessor = PostCSS(
-  PostCSSImport(),
-  PostCSSPresetEnv({ stage: 3 }),
-  CSSNano());
+  const sourceCSS = fs.readFileSync(from, { encoding: 'utf8' });
 
-const sourceCSS = fs.readFileSync('src/style.pcss', { encoding: 'utf8' });
+  cssProcessor.process(sourceCSS, { from, to })
+    .then(result => {
+      fs.writeFileSync(to, result.css, { encoding: 'utf8' });
+      if (result.map) {
+        fs.writeFileSync(`${to}.map`, result.map, { encoding: 'utf8' });
+      }
+    });
+}
 
-cssProcessor.process(sourceCSS, { from: 'src/style.pcss', to: 'dist/style.css'})
-  .then(result => {
-    fs.writeFileSync('dist/style.css', result.css, { encoding: 'utf8' });
-    if (result.map) {
-      fs.writeFileSync('dist/style.css.map', result.map, { encoding: 'utf8' });
-    }
-  });
+console.info(" - Building CSS");
+buildCSS('src/style.pcss', 'dist/style.css');
